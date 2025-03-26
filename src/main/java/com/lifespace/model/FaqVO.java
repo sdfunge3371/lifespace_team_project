983fe91a706ec3a0845e5d6866eb2c3b13bb2051
Mia Liu <immiffy40019@gmail.com>
2025-03-26 22:37:00 +0800

    push "FaqRepository" and "FaqVO"

---
+++ src/main/java/com/lifespace/model/FaqVO.java
@@ -0,0 +1,92 @@
+package com.lifespace.model;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity	//要加上@Entity才能成為JPA的一個Entity類別
+@Table(name = "faq")
+public class FaqVO {
+	
+	@Id
+	@Column(name = "faq_id")
+	@GeneratedValue(generator = "faq_id")
+	@GenericGenerator(name = "faq_id", strategy = "com.lifespace.util.FaqCustomStringIdGenerator")
+	private String faqId;
+	
+	@Column(name = "admin_id")
+	private String adminId;
+	
+	@Column(name = "faq_ask", nullable = false)
+	private String faqAsk;
+	
+	@Column(name = "faq_answer", nullable = false)
+	private String faqAnswer;
+	
+	@Column(name = "faq_status", nullable = false)
+	private Integer faqStatus;
+	
+	@Column(name = "created_time")
+	private Timestamp createTime; 
+	
+
+
+	public FaqVO() {
+		super();
+		
+	}
+	public FaqVO(String faqId, String adminId, String faqAsk, String faqAnswer, Integer faqStatus, Timestamp createTime) {
+		super();
+		this.faqId = faqId;
+		this.adminId = adminId;
+		this.faqAsk = faqAsk;
+		this.faqAnswer = faqAnswer;
+		this.faqStatus = faqStatus;
+		this.createTime = createTime;
+	}
+	
+	
+	
+	public String getFaqId() {
+		return faqId;
+	}
+	public void setFaqId(String faqId) {
+		this.faqId = faqId;
+	}
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getFaqAsk() {
+		return faqAsk;
+	}
+	public void setFaqAsk(String faqAsk) {
+		this.faqAsk = faqAsk;
+	}
+	public String getFaqAnswer() {
+		return faqAnswer;
+	}
+	public void setFaqAnswer(String faqAnswer) {
+		this.faqAnswer = faqAnswer;
+	}
+	public Integer getFaqStatus() {
+		return faqStatus;
+	}
+	public void setFaqStatus(Integer faqStatus) {
+		this.faqStatus = faqStatus;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+}
---
+++ src/main/java/com/lifespace/repository/FaqRepository.java
@@ -0,0 +1,16 @@
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+
+package com.lifespace.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.lifespace.model.FaqVO;
+
+
+@Repository
+public interface FaqRepository extends JpaRepository<FaqVO, String> {
+    
+//    String findTopByOrderByFaqIdDesc();
+}
