0f80a3b36388a34690e1b1d71eb546af4345a755
yen <adni0754686@gmail.com>
2025-04-01 16:31:27 +0800

    新增空間評論到OrdersService、OrdersController、新增dto(SpaceCommentRequest)

--- src/main/java/com/lifespace/entity/SpaceCommentPhoto.java
+++ src/main/java/com/lifespace/entity/SpaceCommentPhoto.java
@@ -4,47 +4,58 @@ import jakarta.persistence.*;
 
 import java.sql.Timestamp;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 @Table(name = "space_comment_photo")
 public class SpaceCommentPhoto {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @Column(name= "space_comment_photo_id")
+    @Column(name= "space_comment_photo_id", updatable = false)
+	@GeneratedValue(generator = "space_comment_photo_id")
+	@GenericGenerator(name = "space_comment_photo_id", strategy = "com.lifespace.util.SpaceCommentPhotoCustomStringIdGenerator")
     private String spaceCommentPhotoId;
 
     @Column(name = "space_photo")
-    private byte[] spacePhoto;
+    private String spacePhoto;
 
     @Column(name = "created_time")
     private Timestamp createdTime;
 
     @ManyToOne
-    @JoinColumn(name = "order_id")
+    @JoinColumn(name = "order_id", referencedColumnName = "order_id")
     private Orders orders;
 
-    public String getSpaceCommentPhotoId() {
-        return spaceCommentPhotoId;
-    }
+	public String getSpaceCommentPhotoId() {
+		return spaceCommentPhotoId;
+	}
 
-    public void setSpaceCommentPhotoId(String spaceCommentPhotoId) {
-        this.spaceCommentPhotoId = spaceCommentPhotoId;
-    }
+	public void setSpaceCommentPhotoId(String spaceCommentPhotoId) {
+		this.spaceCommentPhotoId = spaceCommentPhotoId;
+	}
 
+	public String getSpacePhoto() {
+		return spacePhoto;
+	}
 
-    public byte[] getSpacePhoto() {
-        return spacePhoto;
-    }
+	public void setSpacePhoto(String spacePhoto) {
+		this.spacePhoto = spacePhoto;
+	}
 
-    public void setSpacePhoto(byte[] spacePhoto) {
-        this.spacePhoto = spacePhoto;
-    }
-
-    public Timestamp getCreatedTime() {
+	public Timestamp getCreatedTime() {
         return createdTime;
     }
 
     public void setCreatedTime(Timestamp createdTime) {
         this.createdTime = createdTime;
     }
+
+	public Orders getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Orders orders) {
+		this.orders = orders;
+	}
 }
