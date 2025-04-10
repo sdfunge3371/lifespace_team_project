eb07821a0367d433f2b450bd3175c45f558239df
micky <mickylumc@gmail.com>
2025-04-10 21:25:06 +0800

    新增admin的SessionUtil

--- src/main/java/com/lifespace/SessionUtils.java
+++ src/main/java/com/lifespace/SessionUtils.java
@@ -5,22 +5,45 @@ import jakarta.servlet.http.HttpSession;
 public class SessionUtils {
 
 	   private static final String MEMBER_SESSION_KEY = "loginMember";
+	   private static final String ADMIN_SESSION_KEY = "loginAdmin";
 	  
-	    // 取得會員 ID
+	   //------------------------會員---------------------------------- 
+	   // 取得會員 ID
 	    public static String getLoginMemberId(HttpSession session) {
 	        Object memberId = session.getAttribute(MEMBER_SESSION_KEY);
 	        return memberId != null ? memberId.toString() : null;
 	    }
 	    
 	    // 判斷會員是否已登入
-	    public static boolean isLogin(HttpSession session) {
+	    public static boolean isMemberLogin(HttpSession session) {
 	        return session.getAttribute(MEMBER_SESSION_KEY) != null;
 	    }
 
 	    // 登出時移除登入資料
-	    public static void removeLogin(HttpSession session) {
+	    public static void removeMemberLogin(HttpSession session) {
 	        session.removeAttribute(MEMBER_SESSION_KEY);
 	    }
-	
+	    
+	    
+	  //------------------------管理員---------------------------------- 
+	 // 取得管理員 ID
+	    public static String getLoginAdminId(HttpSession session) {
+	        Object adminId = session.getAttribute(ADMIN_SESSION_KEY);
+	        return adminId != null ? adminId.toString() : null;
+	    }
+	    
+	    // 判斷會員是否已登入
+	    public static boolean isAdminLogin(HttpSession session) {
+	        return session.getAttribute(ADMIN_SESSION_KEY) != null;
+	    }
+
+	    // 登出時移除登入資料
+	    public static void removeAdminLogin(HttpSession session) {
+	        session.removeAttribute(ADMIN_SESSION_KEY);
+	    }
+	    
+	    
+	    
+	    
 	
 }
